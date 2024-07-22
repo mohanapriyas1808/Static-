75e6b6dad5d8731527e8c6dd1012fa15cca3f173
mohanapriyas1808 <143476042+mohanapriyas1808@users.noreply.github.com>
2024-07-23 00:53:28 +0530

    Create policy.tf

---
+++ policy.tf
@@ -0,0 +1,26 @@
+# S3 public access
+resource "aws_s3_bucket_public_access_block" "terraform-demo" {
+    bucket = aws_s3_bucket.terraform-demo-1808.id
+  block_public_acls = false
+  block_public_policy = false
+}
+
+# S3 public Read policy
+resource "aws_s3_bucket_policy" "open_access" {
+  bucket = aws_s3_bucket.terraform-demo-1808.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "Public_access"
+    Statement = [
+      {
+        Sid = "IPAllow"
+        Effect = "Allow"
+        Principal = "*"
+        Action = ["s3:GetObject"]
+        Resource = "${aws_s3_bucket.terraform-demo-1808.arn}/*"
+      },
+    ]
+  })
+  depends_on = [ aws_s3_bucket_public_access_block.terraform-demo ]
+}
